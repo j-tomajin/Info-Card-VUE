a7c9fbd3b19331a461b2215e3e0b716651597c16
Jerickson Tomajin <tomajinj@gmail.com>
2023-06-20 20:40:31 +0800

    check other user inputs

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,35 @@
 <template>
     <Cards 
         :user_name="user_name"
-        :card_num="card_num" />
+        :card_num="card_num"
+        :isValidCardNumber="isValidCardNumber" 
+        :isValidCardNumberLength="isValidCardNumberLength"
+        :month="month"
+        :year="year"
+        :cvc="cvc" />
 
     <InputField
         :isValidName="isValidName"
         @check-user-name="checkUserName"
         
         :isValidCardNumber="isValidCardNumber"
-        @check-card-number="checkCardNumer"/>
+        :isValidCardNumberLength="isValidCardNumberLength"
+        @check-card-number="checkCardNumer"
+        
+        :isValidMonth="isValidMonth"
+        :isValidYear="isValidYear"
+        :isWithinMonth="isWithinMonth"
+        :isWithinYear="isWithinYear"
+        @check-exp-date="checkExpDate"
+
+        :isValidCvc="isValidCvc"
+        :isValidCvcLength="isValidCvcLength"
+        @check-cvc="checkCvc"
+        />
     
     
-    <ThankyouPage />
+    <ThankyouPage
+    class="thankyou" />
 </template>
 
 <script>
@@ -31,7 +49,17 @@
             user_name: '',
             isValidName: true,
             card_num: '',
+            month: '',
+            year: '',
+            cvc: '',
             isValidCardNumber: true,
+            isValidCardNumberLength: true,
+            isValidMonth: true,
+            isValidYear: true,
+            isWithinMonth: true,
+            isWithinYear: true,
+            isValidCvc: true,
+            isValidCvcLength: true,
         }
     },
     methods: {
@@ -45,15 +73,78 @@
         },
 
         checkCardNumer(card_num) {
+            let result = card_num.replace(/\s+/g, "");
+            // console.log(result, result.length)
+
             if(/^\d+$/.test(card_num)) {
-                this.card_num = card_num
-                this.isValidCardNumber = true
-            } else {
                 this.isValidCardNumber = false
+                this.isValidCardNumberLength = true
+            } else {
+                this.isValidCardNumber = true
             }
+            
+            if(result.length != 16) {
+                this.isValidCardNumber = true
+                this.isValidCardNumberLength = false
+            } else {
+                this.isValidCardNumberLength = true
+            }
+
+            if(this.isValidCardNumber && this.isValidCardNumberLength) {
+                this.card_num = card_num
+            }
+        },
+
+        checkExpDate(mm, yy) {
+            const date = new Date().getFullYear()
+            const year = date.toString().slice(-2)
+            
+            if(!mm) {
+                this.isValidMonth = false
+            } else {
+                this.isValidMonth = true
+            }
+            if(!yy) {
+                this.isValidYear = false
+            } else {
+                this.isValidYear = true
+            }
+
+            if(mm > 12) {
+                this.isWithinMonth = false
+            } else {
+                this.isWithinMonth = true
+            }
+            if(yy > year) {
+                this.isWithinYear = false
+            } else {
+                this.isWithinYear = true
+            }
+
+            this.month = mm
+            this.year = yy
+        },
+
+        checkCvc(cvc) {
+            console.log(cvc.length)
+            if(!cvc) {
+                this.isValidCvc = false
+            } else {
+                this.isValidCvc = true
+            }
+
+            if(cvc.length != 3) {
+                this.isValidCvcLength = false
+            } else {
+                this.isValidCvcLength = true
+            }
+
+            this.cvc = cvc
         }
-    }
+    },
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+    
+</style>
