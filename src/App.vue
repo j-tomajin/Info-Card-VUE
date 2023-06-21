2ec76b36452888022fddfc96378e217eb218a02a
Jerickson Tomajin <tomajinj@gmail.com>
2023-06-21 22:57:31 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,43 @@
 <template>
-    <Cards 
-        :user_name="user_name"
-        :card_num="card_num"
-        :isValidCardNumber="isValidCardNumber" 
-        :isValidCardNumberLength="isValidCardNumberLength"
-        :month="month"
-        :year="year"
-        :cvc="cvc" />
-
-    <InputField
-        :isValidName="isValidName"
-        @check-user-name="checkUserName"
-        
-        :isValidCardNumber="isValidCardNumber"
-        :isValidCardNumberLength="isValidCardNumberLength"
-        @check-card-number="checkCardNumer"
-        
-        :isValidMonth="isValidMonth"
-        :isValidYear="isValidYear"
-        :isWithinMonth="isWithinMonth"
-        :isWithinYear="isWithinYear"
-        @check-exp-date="checkExpDate"
-
-        :isValidCvc="isValidCvc"
-        :isValidCvcLength="isValidCvcLength"
-        @check-cvc="checkCvc"
-        />
+    <main class="main">
+        <Cards 
+            class="cards"
     
+            :user_name="user_name"
+            :card_num="card_num"
+            :isValidCardNumber="isValidCardNumber" 
+            :isValidCardNumberLength="isValidCardNumberLength"
+            :month="month"
+            :year="year"
+            :cvc="cvc" />
     
-    <ThankyouPage
-    class="thankyou" />
+        <div class="holder">
+            <InputField
+                :class="[!isDone ? 'show' : 'hidden']"
+    
+                :isValidName="isValidName"
+                @check-user-name="checkUserName"
+                
+                :isValidCardNumber="isValidCardNumber"
+                :isValidCardNumberLength="isValidCardNumberLength"
+                @check-card-number="checkCardNumer"
+                
+                :isValidMonth="isValidMonth"
+                :isValidYear="isValidYear"
+                :isWithinMonth="isWithinMonth"
+                :isWithinYear="isWithinYear"
+                @check-exp-date="checkExpDate"
+        
+                :isValidCvc="isValidCvc"
+                :isValidCvcLength="isValidCvcLength"
+                @check-cvc="checkCvc"
+                />
+            
+            
+            <ThankyouPage
+            :class="[isDone ? 'show' : 'hidden', 'thankyou']" />
+        </div>
+    </main>
 </template>
 
 <script>
@@ -47,11 +55,11 @@
     data: function() {
         return {
             user_name: '',
-            isValidName: true,
             card_num: '',
             month: '',
             year: '',
             cvc: '',
+            isValidName: true,
             isValidCardNumber: true,
             isValidCardNumberLength: true,
             isValidMonth: true,
@@ -60,6 +68,8 @@
             isWithinYear: true,
             isValidCvc: true,
             isValidCvcLength: true,
+
+            isDone: false,
         }
     },
     methods: {
@@ -70,6 +80,8 @@
                 this.user_name = user_name
                 this.isValidName = true
             }
+
+            this.checkIfDone()
         },
 
         checkCardNumer(card_num) {
@@ -93,6 +105,8 @@
             if(this.isValidCardNumber && this.isValidCardNumberLength) {
                 this.card_num = card_num
             }
+
+            this.checkIfDone()
         },
 
         checkExpDate(mm, yy) {
@@ -123,10 +137,10 @@
 
             this.month = mm
             this.year = yy
+            this.checkIfDone()
         },
 
         checkCvc(cvc) {
-            console.log(cvc.length)
             if(!cvc) {
                 this.isValidCvc = false
             } else {
@@ -140,11 +154,55 @@
             }
 
             this.cvc = cvc
+            this.checkIfDone()
+        },
+
+        checkIfDone() {
+            if(this.isValidName && this.isValidCardNumber && this.isValidCardNumberLength && this.isValidMonth && this.isValidYear && this.isValidCvc && this.isValidCvcLength) {
+                this.isDone = true
+                console.log(this.isDone)
+            } else {
+                this.isDone = false
+            }
         }
     },
 }
 </script>
 
 <style lang="scss" scoped>
+    @use './assets/scss/utilities/' as *;
+
+    .main {
+        display: grid;
+        grid-template-columns: 1fr;
+        justify-content: center;
+        gap: clampf(120, 800, 150);
+
+        @include breakpointMin(medium-screen) {
+            grid-template-columns: repeat(2, 1fr);
+            gap: rem(64);
+        }
+    }
+
+    .holder {
+        margin-block: auto;
+        position: relative;
+        overflow: hidden;
+        transition: all 300ms ease-in-out;
+    }
+
+    .show {
+        position: unset;
+    }
     
+    .hidden {
+        position: absolute;
+        width: 100%;
+        top: 0;
+        right: -100%;
+    }
+
+    .cards {
+        margin-block: auto;
+    }
 </style>
